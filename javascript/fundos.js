e4abeae28e94226b46fce7cc55f2e15441c49100
Rkzzin <henriquels5@al.insper.edu.br>
2023-11-25 21:36:32 -0300

    Troca de backgrounds

--- javascript/fundos.js
+++ javascript/fundos.js
@@ -1,13 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Pega o src da imagem clicada
     fundos = document.querySelectorAll('figure img');
+    html = document.querySelector('html');
+
+    // Troca a imagem de fundo pela imagem salva no localStorage
+    html.style.backgroundImage = localStorage.getItem('fundoUrl');
 
     // Adiciona um event listener para cada imagem
     for (let i = 0; i < fundos.length; i++) {
         fundos[i].addEventListener('click', function() {
-            // Pega o src da imagem clicada e salva no localStorage
-            image_src = fundos[i].getAttribute('src');
-            localStorage.setItem('fundo', image_src);
+            // Pega o SRC e o URL da imagem clicada e salva no localStorage
+            localStorage.setItem('fundoUrl', ('url(' + fundos[i].src +')'));
+
+            // Troca a imagem de fundo pela imagem salva no localStorage ao selecionar uma nova
+            html.style.backgroundImage = localStorage.getItem('fundoUrl');
         });
     };
 });
--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Troca a imagem de fundo pela imagem salva no localStorage
     html = document.querySelector('html');
-    if (localStorage.getItem('fundo')) {
-        html.style.backgroundImage = 'url(' + localStorage.getItem('fundo') + ')';
+    if (localStorage.getItem('fundoUrl')) {
+        html.style.backgroundImage = localStorage.getItem('fundoUrl');
     };
-    
 });
