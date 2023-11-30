d355f6d61c0cf78d76d10e8d1678af8854aa0447
thevitorhideki <7vitorgomez@gmail.com>
2023-11-30 11:17:13 -0300

    Correção

--- javascript/index.js
+++ javascript/index.js
@@ -1,7 +1,11 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     // Troca a imagem de fundo pela imagem salva no localStorage
     html = document.querySelector('html');
+
     if (localStorage.getItem('fundoUrl')) {
         html.style.backgroundImage = localStorage.getItem('fundoUrl');
-    };
+    } else {
+        localStorage.setItem('fundoUrl', 'url(http://127.0.0.1:5500/img/novo_fundo4.jpg)');
+        html.style.backgroundImage = localStorage.getItem('fundoUrl');
+    }
 });
