d43723bd21bd31f2c40fcdad48fe9d33ff6c9da6
Rkzzin <henriquels5@al.insper.edu.br>
2023-12-02 20:53:58 -0300

    Lógica que muda o título do chat que você entra

--- javascript/grupos.js
+++ javascript/grupos.js
@@ -1,19 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Adicionar a mensagem digitada pelo usuário no campo de texto
-    chat = document.querySelector('.chat');
-    envio = document.querySelector('img[class="envio"]');
+    // Seleciona todos os grupos
+    titulo_grupo = document.querySelectorAll('section');
 
-    // Verifica se apertou o botão de enviar
-    envio.addEventListener('click', function() {
-        mensagem = document.querySelector('input[type="text"]');
-        if (mensagem.value != '') {
+    // Adiciona o evento de clique em cada grupo
+    titulo_grupo.forEach(function(elemento) {
+        elemento.addEventListener('click', function() {
+            // Seleciona o h2 filho do grupo clicado e salva no localStorage
+            h2 = this.querySelector('h2').innerHTML;
+            localStorage.setItem('grupo_titulo', h2);
+            }
+        );
 
-            nova_mesagem = document.createElement('p');
-            nova_mesagem.innerHTML = mensagem.value;
-            nova_mesagem.classList.add('user');
-
-            chat.appendChild(nova_mesagem);
-            mensagem.value = '';
-        }
     });
 });
