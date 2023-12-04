1e466336fa20d50071f2fddeee1770e3758d3d90
Rkzzin <henriquels5@al.insper.edu.br>
2023-12-04 11:15:43 -0300

    Data da tela inicial

--- javascript/index.js
+++ javascript/index.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', function () {
     // Troca a imagem de fundo pela imagem salva no localStorage
     html = document.querySelector('html');
+    
 
     if (localStorage.getItem('fundoUrl')) {
         html.style.backgroundImage = localStorage.getItem('fundoUrl');
@@ -8,4 +9,16 @@ document.addEventListener('DOMContentLoaded', function () {
         localStorage.setItem('fundoUrl', 'url(http://127.0.0.1:5500/img/novo_fundo4.jpg)');
         html.style.backgroundImage = localStorage.getItem('fundoUrl');
     }
+
+    // Função para pegar o dia, mes e ano atual e colocar nas informações
+    var date = new Date();
+    var dia = date.getDate();
+    var ano = date.getFullYear();
+    
+    var mes = date.getMonth();
+    const meses = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembo', 'Outubro', 'Novembro', 'Dezembro'];
+    mes = meses[mes];
+
+    data = document.querySelector('p');
+    data.innerHTML = dia + ' de ' + mes + ' de ' + ano;
 });
